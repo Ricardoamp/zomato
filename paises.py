4afd183d7b2a57de236ebd9d3885d04f49aeb83d
Ricardoamp <102176416+Ricardoamp@users.noreply.github.com>
2023-03-06 12:59:01 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -104,7 +104,7 @@ tab1, tab2, tab3 = st.tabs(['Geral'  + emoji.emojize(':earth_africa:'),'Pais'+ e
 
 with tab1:
     
-    st.markdown('##### A empresa Fome Zero é uma marketplace de restaurantes. Ou seja, seu core business é facilitar o encontro e negociações de clientes e restaurantes. Os restaurantes fazem o cadastro dentro da plataforma da Fome Zero, que disponibiliza informações como endereço, tipo de culinária servida, se possui reservas, se faz entregas e também uma nota de avaliação dos serviços e produtos do restaurante, dentre outras informações.')
+    st.markdown('##### Esse projeto é de um marketplace de restaurantes. Ou seja, seu core business é facilitar o encontro e negociações de clientes e restaurantes. Os restaurantes fazem o cadastro dentro da plataforma, que disponibiliza informações como endereço, tipo de culinária servida, se possui reservas, se faz entregas e também uma nota de avaliação dos serviços e produtos do restaurante, dentre outras informações.')
     st.markdown("""---""")
     
     
